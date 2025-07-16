807322860745b2939fdb8f0a96068e1fd86efb21
fvguajeXIII <f.vellucci13@outlook.it>
2025-07-16 12:51:00 +0200

    Esercizio 2

--- js/script.js
+++ js/script.js
@@ -2,3 +2,8 @@
 
 let numero = 5;
 console.log(numero);
+
+//Dichiarare una costante chiamata "PI" e assegnarle il valore di 3.14. Stampare il valore della costante sulla console.
+
+let pi = 3.14;
+console.log(pi);
