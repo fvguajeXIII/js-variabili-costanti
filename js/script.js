83cf9bc769a8e0c401d502408cb171aa46557e68
fvguajeXIII <f.vellucci13@outlook.it>
2025-07-16 12:52:23 +0200

    Esercizio 3

--- js/script.js
+++ js/script.js
@@ -7,3 +7,10 @@ console.log(numero);
 
 let pi = 3.14;
 console.log(pi);
+
+//Dichiarare una variabile chiamata "nome" e assegnarle il valore "Marco". Successivamente, modificare il valore della variabile in "Luca". Stampare il nuovo valore sulla console.
+
+let nome = `Marco`;
+nome = `Luca`;
+console.log(nome);
+
