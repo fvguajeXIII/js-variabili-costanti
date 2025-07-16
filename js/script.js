304130d3d3d2f1adc9b12488b52c46482f6a9bb8
fvguajeXIII <f.vellucci13@outlook.it>
2025-07-16 12:54:44 +0200

    Esercizio 6

--- js/script.js
+++ js/script.js
@@ -23,3 +23,8 @@ console.log(saluto);
 
 let vero = true;
 console.log(vero);
+
+//Dichiarare una variabile chiamata "numeri" e assegnarle un array contenente i numeri da 1 a 5. Stampare l'array sulla console.
+
+let numeri = [1,2,3,4,5];
+console.log(numeri);
